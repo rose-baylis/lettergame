da173bfacbcae0aee923d4961594b757128eb673
rose-baylis <rose.l.baylis@gmail.com>
2022-03-14 18:07:03 +1300

    looping over intial word

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+  const word = ["wine"]
+  const numberOfLetters = word[0].length
+  const arrayOfLetters = word[0].split("")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi Rose</Text>
+      {arrayOfLetters.map((letter) => {
+        return <Text>{letter}</Text>
+      })}
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
